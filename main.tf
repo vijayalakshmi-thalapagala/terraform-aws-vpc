a8b6c7a8f3a4e1af8c55aa68ab31a989e641c140
Vijayalakshmi Thalapagala <thalapav@amdocs.com>
2026-03-08 23:47:53 +0530

    actions

--- main.tf
+++ main.tf
@@ -12,12 +12,17 @@ resource "aws_internet_gateway" "main" {
     tags = local.igw_final_tags
 }
 
-resource "aws_subnet" "public" {
+# public subnets
+ resource "aws_subnet" "public" {
     count = length(var.public_subnet_cidrs)
     vpc_id = aws_vpc.main.id
     cidr_block = var.public_subnet_cidrs[count.index]
     
-    tags = {
-        Name = "Main"
-    }
-}
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+        }
+        var.public_subnet_tags
+    )
+ }
